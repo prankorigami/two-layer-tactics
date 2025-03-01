11195cea97096fba5240004ab14b3155975ffd29
jross5 <jross5@ncsu.edu>
2025-03-01 16:25:58 -0500

    Created main menu
    Created main menu / title screen

---
+++ src/scenes/UI/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/levels/Level.tscn")
+	
+
+
+func _on_help_pressed() -> void:
+	print("Help Pressed")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
